f0d2b16b0c5d1c8fae87d694833e6b5b604a5316
BraidonH <Braidonthegreat@gmail.com>
2024-04-21 13:40:12 -0500

    set pug as default view, added render to root

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('views', './views/pug')
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 app.route('/').get((req, res) => {
   res.render('index.pug')
 });
